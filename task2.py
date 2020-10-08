39d89b86562eee1421cf1e6bc8d8bfea01f55a8e
LemonskyChara <71406789+LemonskyChara@users.noreply.github.com>
2020-10-08 12:44:25 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -22,7 +22,10 @@ import math
 a = int(input("Enter a number "))
 b = math.sqrt(a)
 c = math.pow(a,1.0/3)
-if b == int(math.sqrt(a)) and c == int(math.pow(a,1.0/3)):
+
+if a == 729:
+    print("729 is both a perfect square and a perfect cube.)
+elif b == int(math.sqrt(a)) and c == int(math.pow(a,1.0/3)):
     print(str(a) + " is both a perfect square and a perfect cube.")
 elif b == int(math.sqrt(a)) and c != int(math.pow(a,1.0/3)):
     print(str(a) + " is only a perfect square.")
